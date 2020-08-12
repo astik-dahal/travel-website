599d44e9d2176e067ab5cdffe756d5bd27263946
ki-tsa <37465295+ki-tsa@users.noreply.github.com>
2020-08-12 19:53:05 +0545

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,21 @@
+    const header = document.querySelector('.header');
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    if (top >= 110){
+        header.classList.add('header-active')
+    }else{
+        header.classList.remove('header-active')
+    }
+}
+
+
+const navToggle = document.getElementsByClassName('nav-toggle')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+navToggle.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+navToggle.addEventListener('click', () => {
+   nav-toggle.classList.toggle('active')
+  })
